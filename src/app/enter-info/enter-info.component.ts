3fd7b10b18ffc6a2099f19cbfce2b83e5be46dc0
admin <admin@0336-MacBook-Pro.local>
2017-03-19 15:10:57 -0700

    Second Commit: Display trigger

--- src/app/enter-info/enter-info.component.ts
+++ src/app/enter-info/enter-info.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 import { Deduction } from '../deduction';
 import { DeductionsService } from '../deductions.service';
@@ -9,6 +9,7 @@ import { DeductionsService } from '../deductions.service';
   styleUrls: ['./enter-info.component.css']
 })
 export class EnterInfoComponent  {
+  @Output() reloadData = new EventEmitter()
   private value: number;
   private text: string;
   private typetext: string;
@@ -56,8 +57,14 @@ export class EnterInfoComponent  {
   }
 
   submitDeduction() {
-    var deduction = new Deduction(this.selectedType, this.text, this.value);
-    console.log("description: " + this.text + "\nAmount: " + this.value + "\nType: " + this.selectedType)
-    this.deductions.addDeduction(deduction, this.selectedMonth);
+    if((this.text) && (this.value) && (this.selectedMonth) && (this.selectedType)){
+      var deduction = new Deduction(this.selectedType, this.text, this.value);
+      console.log("description: " + this.text + "\nAmount: " + this.value + "\nType: " + this.selectedType)
+      this.deductions.addDeduction(deduction, this.selectedMonth);
+      this.reloadData.emit(null);
+    }
+    else {
+      alert("Please complete all values");
+    }
   }
 }
